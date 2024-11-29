e6a9f257d4fb2950df6bcd90fb3863ca57121f49
Tobías Hernández <137558648+tobhdz@users.noreply.github.com>
2024-11-29 13:31:28 -0300

    finanzas refactor

--- frontend/src/componentes/paginas-app/Finanzas.js
+++ frontend/src/componentes/paginas-app/Finanzas.js
@@ -15,16 +15,23 @@ export default function Finanzas() {
         <div className="finanzas-container">
             <div className="finanzas-box">
                 <h1>Finanzas</h1>
-                <div className="btn-container">
-                    <Boton className="boton1" type="button" title="Gastos" link="/gastos" />
-                    <Boton className="boton1" type="button" title={<><FontAwesomeIcon icon={faCirclePlus} /> Crear gasto</>} link="/creargasto" />
-                </div>
 
                 <div className="card-container">
                     <h2>Cuentas por pagar</h2>
-                    <CardGastos title="Atrasados" tipo={getGastosVencidos()} className="card-item"></CardGastos>
-                    <CardGastos title="Vence hoy" tipo={getGastosPorVencer(0)} className="card-item"></CardGastos>
-                    <CardGastos title="Proximos 7 días" tipo={getGastosPorVencer(7)} className="card-item"></CardGastos>
+                    <div className="finanzas-card">
+                        <div className="finanzas-info">
+                            <h5>Nombre del gasto</h5>
+                            <div className="finanzas-detalles">
+                                <span>Proyecto: Nombre del proyecto</span>
+                                <span>Acreedor: Nombre del acreedor</span>
+                            </div>
+                            <p className="subtotal-gasto">10</p>
+                        </div>
+                        <div className="finanzas-botones">
+                            <button>Pagar</button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
